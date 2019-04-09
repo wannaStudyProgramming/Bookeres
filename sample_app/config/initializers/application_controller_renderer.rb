dc2378cf4d1e459bf6177c80270120bf8cc4c2af
Ange Magi <lfnj.k.kohei@gmail.com>
2019-04-09 13:50:33 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fee61aa1ba06b09ebc3af380f86b8f21c5d65b32450d98d2a30ceb2400ccedb337e0c6215c36813aba7bd5be5105120e55326b9f5be716d230ad361252bd93c2'
