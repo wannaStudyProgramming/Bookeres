d594b8edbc4fb05eacf9d41acc3e14ce334e4224
Ange Magi <lfnj.k.kohei@gmail.com>
2019-04-09 13:05:02 +0900

    [Finish]3-7

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -6,7 +6,26 @@ class PostsController < ApplicationController
   def create
 	post=Post.new(post_params)
 	post.save
-	redirect_to "/top"
+	redirect_to post_path(post.id) #詳細画面へのリダイレクト
+  end
+
+  def index
+	@posts=Post.all
+  end
+
+  def show
+	@post=Post.find(params[:id])
+  end
+
+  def edit
+	@post=Post.find(params[:id])
+
+  end
+
+  def update
+	post=Post.find(params[:id])
+	post.update(post_params)
+	redirect_to post_path(post.id)
   end
 
   private
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   get 'posts/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'root#top'
+  get '/top' => 'root#top'
 
-  post "posts" => "posts#create"
+  post "/posts" => "posts#create"
 
+  get "/posts" => "posts#index"
+
+  get"/posts/:id" => "posts#show", as: "post"
+
+  get "posts/:id/edit" => "posts#edit", as: "edit_post"
+
+  patch "posts/:id" => "posts#update", as:"update_post"
 end
